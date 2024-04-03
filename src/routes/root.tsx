6c65f92610d940b23b1a2dcfcfeec86f87cd8b2f
Rask467 <95650694+Rask467@users.noreply.github.com>
2024-04-03 13:05:16 -0600

    Changes

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -24,28 +24,28 @@ export default function Root() {
           Sign Up
         </Link>
       </div>
-      <div className="bg-green-600 flex flex-col p-48 w-100 mt-20 justify-start items-start md:justify-center md:items-center">
-        <h1 className="font-serif text-white text-6xl md:text-8xl">Pomerene</h1>
-        <h2 className="text-yellow-400 text-xl uppercase" style={{wordSpacing: "0.43rem"}}>Triple Entry International Trade</h2>
+      <div className="bg-green-600 flex flex-col w-full py-48 px-8 mt-20 justify-center items-center">
+        <h1 className="font-serif text-white w-full md:text-center text-6xl md:text-8xl">Pomerene</h1>
+        <h2 className="text-yellow-400 text-xl w-full md:text-center uppercase" style={{wordSpacing: "0.43rem"}}>Triple Entry International Trade</h2>
       </div>
       <div className="p-48 rounded flex flex-col w-100 max-h-96 mt-4 md:mt-6 justify-center items-center">
         <h1 className="font-serif text-black text-5xl">Securing $13 Trillion in Global Trade</h1>
       </div>
-      <div className="p-8 bg-green-600 text-white flex flex-col min-h-96 w-100 mt-20 md:mt-6 justify-center items-center">
-        <div className="w-full md:flex-row flex-col mb-4 flex justify-between xl:justify-around items-center">
+      <div className="p-8 font-sans bg-green-600 text-white flex flex-col min-h-96 w-100 mt-20 md:mt-6 justify-center items-center">
+        <div className="w-full md:flex-row md:pb-10 flex-col flex justify-between xl:justify-around items-center">
           <div className="w-1/2 xl:w-1/3 mt-6">
-            <h3 className="font-serif text-2xl">Shared Data Standard.</h3>
+            <h3 className="text-2xl">Shared Data Standard.</h3>
           </div>
           <div className="w-1/2 xl:w-1/3 mt-6">
-            <h3 className="font-serif text-2xl">IoT Network of Connected Devices.</h3>
+            <h3 className="text-2xl">IoT Network of Connected Devices.</h3>
           </div>
         </div>
         <div className="w-full flex md:flex-row flex-col justify-between xl:justify-around items-center">
           <div className="w-1/2 xl:w-1/3 mt-6">
-            <h3 className="font-serif text-2xl">Base Layer for a Smart Economic Stack.</h3>
+            <h3 className="text-2xl">Base Layer for a Smart Economic Stack.</h3>
           </div>
           <div className="w-1/2 xl:w-1/3 mt-6">
-            <h3 className="font-serif text-2xl">Authenticated Inventory Tracking System.</h3>
+            <h3 className="text-2xl">Authenticated Inventory Tracking System.</h3>
           </div>
         </div>
       </div>
