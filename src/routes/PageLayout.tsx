7416b100e830931deea2b336addc85184fb284da
russell-brouillard <rbrouillard@crunchtime.com>
2024-04-02 20:56:33 -0600

    position

---
+++ src/routes/PageLayout.tsx
@@ -0,0 +1,29 @@
+import { Outlet } from "react-router-dom";
+import { MainNav } from "@/components/mainNav";
+
+import { UserNav } from "@/components/userNav";
+import { useAuth } from "@/contexts/useAuth";
+
+const PageLayout = () => {
+  const { currentUser } = useAuth();
+
+  return (
+    <div>
+      <div className="border-b">
+        <div className="flex h-16 items-center px-3">
+          <MainNav />
+
+          <div className="ml-auto flex items-center space-x-4">
+            <UserNav user={currentUser} />
+          </div>
+        </div>
+      </div>
+      <main>
+        <Outlet />
+      </main>
+      <footer>{/* Footer content */}</footer>
+    </div>
+  );
+};
+
+export default PageLayout;
--- src/routes/items/items.tsx
+++ src/routes/items/items.tsx
@@ -43,10 +43,6 @@ export default function Items() {
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
   const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
   const [rowSelection, setRowSelection] = useState({});
-  // const [loadingReport, setLoadingReport] = useState(false);
-  // const items = useLoaderData() as ItemType[];
-  // const items =  itemLoader();
-  // const [loadingReport] = useState(false);
   const [items, setItems] = useState<ItemType[]>([]);
   const { currentUser } = useAuth();
   const [progress, setProgress] = useState(13);
@@ -63,8 +59,6 @@ export default function Items() {
     const fetchItems = async () => {
       setLoadingData(true);
       try {
-        // Assuming currentUser comes from your authentication context
-        // and it might be null initially until the auth state is resolved.
         if (!currentUser) {
           console.log("No current user. Skipping fetch.");
           return;
@@ -123,47 +117,10 @@ export default function Items() {
     },
   });
 
-  // const downloadFile = ({ data, fileName, fileType }) => {
-  //   const blob = new Blob([data], { type: fileType })
-  //   const a = document.createElement('a')
-  //   a.download = fileName
-  //   a.href = window.URL.createObjectURL(blob)
-  //   const clickEvt = new MouseEvent('click', {
-  //     view: window,
-  //     bubbles: true,
-  //     cancelable: true,
-  //   })
-  //   a.dispatchEvent(clickEvt)
-  //   a.remove()
-  // }
-
-  // async function downloadReport(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-  //   setLoadingReport(true);
-  //   try {
-  //     e.preventDefault()
-  //     // Headers for each column
-  //     let headers = ['Id,Name,Coordinates,PublicKey, CreatedAt, Status']
-  //     let itemsCsv = items.reduce((acc, item: any) => {
-  //       const { id, name, coordinates, publicKey, createdAt, status } = item
-  //       acc.push([id, name, coordinates, publicKey, createdAt, status].join(','))
-  //       return acc
-  //     }, [])
-  //     downloadFile({
-  //       data: [...headers, ...itemsCsv].join('\n'),
-  //       fileName: 'items.csv',
-  //       fileType: 'text/csv',
-  //     })
-  //   } catch (error) {
-
-  //   } finally {
-  //     setLoadingReport(false)
-  //   }
-  // }
-
   return (
     <>
       <div className="flex-col md:flex">
-        <div className="flex-1 space-y-4 p-8 pt-6">
+      <div className="flex-1 space-y-4 p-4 pt-6">
           <div className="flex items-center justify-between space-y-2">
             <h2 className="text-3xl font-bold tracking-tight">Items</h2>
             <div className="flex items-center space-x-2">
@@ -172,87 +129,84 @@ export default function Items() {
               </Button>
             </div>
           </div>
-         
+
           {/* <div className="rounded-md border"> */}
-            <Table>
-              <TableHeader>
-                {table.getHeaderGroups().map((headerGroup) => (
-                  <TableRow key={headerGroup.id}>
-                    {headerGroup.headers.map((header) => {
-                      return (
-                        <TableHead key={header.id}>
-                          {header.isPlaceholder
-                            ? null
-                            : flexRender(
-                                header.column.columnDef.header,
-                                header.getContext()
-                              )}
-                        </TableHead>
-                      );
-                    })}
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </TableHead>
+                    );
+                  })}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
                   </TableRow>
-                ))}
-              </TableHeader>
-              <TableBody>
-                {table.getRowModel().rows?.length ? (
-                  table.getRowModel().rows.map((row) => (
-                    <TableRow
-                      key={row.id}
-                      data-state={row.getIsSelected() && "selected"}
-                    >
-                      {row.getVisibleCells().map((cell) => (
-                        <TableCell key={cell.id}>
-                          {flexRender(
-                            cell.column.columnDef.cell,
-                            cell.getContext()
-                          )}
-                        </TableCell>
-                      ))}
-                    </TableRow>
-                  ))
-                ) : (
-                  <TableRow>
-                    <TableCell
-                      colSpan={columns?.length}
-                      className="h-24 text-center"
-                    >
-                      {!loadingData ? (
-                        <p>No Items. Click "Create Item" Button to Create Pallet Tag (check if user has sol)</p>
-                      ) : (
-                        <Progress value={progress} className="w-[60%]" />
-                      )}
-                    </TableCell>
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-          </div>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={columns?.length}
+                    className="h-24 text-center"
+                  >
+                    {!loadingData ? (
+                      <p>
+                        No Items. Click "Create Item" Button to Create Pallet
+                        Tag (check if user has sol)
+                      </p>
+                    ) : (
+                      <Progress value={progress} className="w-[60%]" />
+                    )}
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
           <div className="flex items-center justify-end space-x-2 py-4">
-            <div className="flex-1 text-sm text-muted-foreground">
-              {table.getFilteredSelectedRowModel().rows?.length} of{" "}
-              {table.getFilteredRowModel().rows?.length} row(s) selected.
-            </div>
-            <div className="space-x-2">
-              <Button
-                variant="outline"
-                size="sm"
-                onClick={() => table.previousPage()}
-                disabled={!table.getCanPreviousPage()}
-              >
-                Previous
-              </Button>
-              <Button
-                variant="outline"
-                size="sm"
-                onClick={() => table.nextPage()}
-                disabled={!table.getCanNextPage()}
-              >
-                Next
-              </Button>
-            </div>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              Previous
+            </Button>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              Next
+            </Button>
           </div>
         </div>
-        <Toaster />
+      </div>
+      <Toaster />
       {/* </div> */}
     </>
   );
