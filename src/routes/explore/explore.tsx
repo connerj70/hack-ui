dbb0d72a4bceaf61d69041c5e7439d65e513412a
russell-brouillard <rbrouillard@crunchtime.com>
2024-05-07 13:13:05 -0600

    save

--- src/routes/explore/explore.tsx
+++ src/routes/explore/explore.tsx
@@ -1,19 +1,26 @@
 import MapComponent from "@/components/MapComponent";
-import { useEffect } from "react";
+import { ItemType } from "@/types/itemTypes";
+import { useEffect, useState } from "react";
 
 export default function Explore() {
+  const [items, setItems] = useState<ItemType[]>([]);
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(
-        "https://api.coingecko.com/api/v3/coins/list"
+        `${import.meta.env.VITE_API_URL}/item/explore`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
       );
       const data = await response.json();
-      console.log(data);
+      console.log(data.items);
+      setItems(data.items);
     }
 
     fetchData();
-  });
-  return (
-    <MapComponent data={[]} width={"100vw"} height={"100vh"} />
-  );
+  }, []);
+  return <MapComponent data={items} width={"100vw"} height={"100vh"} />;
 }
