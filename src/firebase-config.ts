7115dfc74f5f73cc3afe4bf5ed9e04d36306fd5f
Russell Brouillard <rssll.brllrd@gmail.com>
2024-07-23 16:39:44 -0600

    test

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,15 +1,14 @@
 import { getAuth } from "@firebase/auth";
 import { initializeApp } from "firebase/app";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA2tdOulU73ADVdPhrnxQYt570L4ZBRQFQ",
-  authDomain: "pomerene-dev.firebaseapp.com",
-  projectId: "pomerene-dev",
-  storageBucket: "pomerene-dev.appspot.com",
-  messagingSenderId: "844147711427",
-  appId: "1:844147711427:web:2386f0061923f540bdf176",
-  measurementId: "G-6T3CY94SZB"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
