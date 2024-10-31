000fcbc2045eef8c9e01413411312b32ef1df021
russell-linkeen <russell.brouillard@linkeen.com>
2024-10-30 23:51:10 -0600

    .id

--- src/routes/scanner/scanners.tsx
+++ src/routes/scanner/scanners.tsx
@@ -122,20 +122,13 @@ const Scanners: FC = () => {
           return;
         }
 
-        const scannerItems: ScannerType[] = body.scanners
-          .filter((scanner: ScannerType) => scanner.id && scanner.id.id) // Ensure scanner has an id object with id string
-          .map((scanner: ScannerType) => ({
-            ...scanner,
-            selected: selectedScanner?.id.id === scanner.id.id, // Corrected comparison
-          }));
-
-        setScanners(scannerItems);
+        setScanners(body.scanners);
         setLoadingData(false);
       } catch (error) {
         console.error("An unexpected error occurred:", error);
         toast({
           title: "Error",
-          description: "An unexpected error occurred.",
+          description: `An unexpected error occurred. ${error}`,
           variant: "destructive",
         });
         setLoadingData(false);
